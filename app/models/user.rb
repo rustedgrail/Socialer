2c61b6099173e011576c8a0ba0a5e93064638bae
David Newell <beta@alumni.rice.edu>
2011-07-20 23:40:53 -0400

    David: validates presence of fields on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  validates :first_name, :presence => true
+  validates :last_name, :presence => true
+  validates :email, :presence => true
+  validates :city, :presence => true
 end
