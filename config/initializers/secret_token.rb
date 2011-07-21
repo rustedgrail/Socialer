ba21970ca7233392001f385b4b629cfa8d83fddb
David Newell <beta@alumni.rice.edu>
2011-07-20 20:41:27 -0400

    David: first commit of socialer  with a user

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Socialer::Application.config.secret_token = '6ea16a67ad9d88b5d6da061417a71485660f9cc1700f6ff2c1c11077d9fe51f0c81387e81cc9e45e67e45e623aa9ccb532a652ccc059cd591eaecfaf453ef267'
